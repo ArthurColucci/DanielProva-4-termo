2917c1101f405b9a947416e48071769b7dc33865
ArthurKoluk <arthur.colucci@aluno.ifsp.edu.br>
2022-10-04 20:18:39 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaRosa/Data/AppCont.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DanielProva.Models;
+
+namespace DanielProva.Data
+{
+    public class AppCont : DbContext
+    {
+        public AppCont(DbContextOptions<AppCont> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
